460d5a51f767539af0d1c87a5a3a20c6f103477c
Joao Paulo <vainawebcpx@gmail.com>
2019-12-18 12:31:47 -0300

    subindo babidi

---
+++ src/component/Landing.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import '../App.css';
+
+class Landing extends React.Component {
+    render (){
+        return(
+            <div className='landing'>
+                <h1>I am Babidi of the programation</h1>
+                <marquee scrollamount='30'><img className="babidi" src="https://i.pinimg.com/originals/80/a7/4e/80a74ec3ef1a20083f98440aaeca8513.png" alt=''/></marquee>
+                <p>
+                Babidi é um velho bruxo, de uma raça desconhecida, que desde seu aparecimento na Terra, sete anos após a morte de Cell, tenta reviver ao monstro Majin Boo, uma besta criada magicamente por seu pai, de um poder enorme.
+
+                Babidi é um experiente em magia e em artes escuras e sua técnica principal consiste em controlar mentalmente a todos aqueles lutadores que têm maldade em seu coração, lhes convertendo em Majin´s.
+
+                Com essa técnica consegue controlar a lutadores como Yamu, Spopovitchi, Pui Pui, o monstro Yakon, Vegeta, e inclusive o Rei Demônio Dabura.
+
+                Sua única ambição é o controle do poder total, eliminando a quem tente enfrentar-lhe, bem por médio de seus Majin, ou bem por médio de artes mágicas.
+
+                Possui uma nave espacial dividida em níveis, a cada um dos quais está custodiado por um de seus Majin. No primeiro nível encontra-se Pui Pui, no segundo nível Yakon, e Dabura é o encarregado do terceiro nível. A nave está desenhada para captar toda a energia que desprendem os combates que em seu interior se livram, e a dirigir diretamente para a esfera onde se encontra encerrado Majin Bo.
+
+                Ainda que sabia que seu pai encerrava ao monstro porque não era capaz do controlar, Babidi finalmente consegue sua libertação. Mais tarde, ao igual que seu pai, se enfrenta à desobediência do monstro ao que só consegue dominar levemente com ameaças de voltar ao encerrar. O monstro é tão desobediente que inclusive está a ponto de abandonar a Babidi a sua sorte, quando este é partido ao meio e quase morrido por Piccolo, mas novamente uma ameaça de ser encerrado funciona, e Majin Boo sana ao velho bruxo, e planeja a dominação do universo com seu monstro.
+
+                No entanto, as reiteradas ameaças e as contínuas ordens, terminarão por cansar tanto a Majin Boo, que não suportará ao bruxo, e finalmente o eliminará com suas próprias mãos, ficando assim livre para viver sua própria vida (transformando a todo mundo em chocolate para poder lhos comer). Finalmente este mau vício do monstro, ao igual que seu desobediência, serão controlados por Mr. Satan, a primeira pessoa que realmente se preocupa por ele, e lhe ensina a diferença entre o bem e o mau.
+                </p>
+            </div>
+        )
+    }
+}
+
+export default Landing;
